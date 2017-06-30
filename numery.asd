259bf89eeb083a1a674f35a58aa8824eb2e926da
Ignacy Moryc <imoryc@protonmail.com>
2017-06-30 16:34:11 +0200

    Specs for absolute error

--- numery.asd
+++ numery.asd
@@ -5,6 +5,6 @@
   :author "Your Name <your.name@example.com>"
   :license "Specify license here"
   :serial t
+  :depends-on (#:lisp-unit)
   :components ((:file "package")
                (:file "numery")))
-
--- numery.lisp
+++ numery.lisp
@@ -5,18 +5,24 @@
 ;;; "numery" goes here. Hacks and glory await!
 
 (defun absolute-error (value approximation)
+  "Return the absolute error between two numbers"
   (abs
-   (- (coerce value 'float)
-      (coerce approximation 'float))))
+   (- (coerce value 'double-float)
+      (coerce approximation 'double-float))))
 
 (define-test test-absolute-error
-    (assert-equal 0.0 (absolute-error 10 10)))
+  (assert-float-equal 0.0d0 (absolute-error 10 10))
+  (assert-float-equal 0.5d0 (absolute-error -10 -9.5))
+  (assert-float-equal 0.1d0 (absolute-error 10 9.9)))
 
 (defun relative-error (absolute-error approximation)
-  (/ absolute-error (abs approximation)))
-
+  "Return the relative error between two numbers"
+  (abs (if (or (zerop exact) (zerop approximate))
+           (- exact approximate)
+           (/ (- exact approximate) exact))))
 
 ;; To run tests in SLIME use
+;; (ql:quickload "numery")
 ;; (in-package :numery)
 ;; (setq *print-failures* t)
 ;; (run-tests)
