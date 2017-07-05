98f5b1ad9a5d015566c72bd3bbff2d1160db2aaa
Ignacy Moryc <imoryc@protonmail.com>
2017-07-05 16:23:53 +0200

    Compute Alternating Convergent Series Sum

--- numery.lisp
+++ numery.lisp
@@ -25,6 +25,44 @@
   (assert-true (< 0.0105 (relative-error 0.02d0 1.9d0) 0.0106))
   (assert-true (< 0.0444 (relative-error 0.08d0 1.8d0) 0.0445)))
 
+(defun print-float (float)
+  (format t "~10$" float))
+
+(defun factorial (n)
+  "Computes factorial for small n using iterative approach"
+  (check-type n unsigned-byte)
+  (loop for i from 0 to n
+     for factorial = 1 then (* factorial i)
+     finally (return factorial)))
+
+(define-test test-factorial
+  (assert-equal 3628800 (factorial 10))
+  (assert-equal 1 (factorial 0)))
+
+(defun series-1 (max)
+  (loop for n from 0 to max
+     sum (* (expt -1 n) (/ (* 2 n) (factorial (+ n 2))))))
+
+(defvar *epsilon-for-series-1* 0.0000000001)
+(defvar *max-sequence-expansion* 30)
+
+(defun series-sum-1 (series)
+  "Computes estimated sum of alternating convergent series assuming
+  that the sum would be the term n which satisfies condition:
+
+     | term(n) - term(n-1) | < Epsilon
+
+  "
+  (loop for n from 0 to *max-sequence-expansion*
+     for epsilon = (abs (- (funcall series n) (funcall series (+ n 1))))
+     when (< epsilon *epsilon-for-series-1*)
+     return (format nil "Sum ~8$ at sequence number ~d ~%" (funcall series n) n)))
+
+(define-test test-series-sum-1
+  (assert-true (string=
+   (format nil "Sum -0.20727664 at sequence number 12 ~%")
+   (series-sum-1 #'series-1))))
+
 ;; To run tests in SLIME use
 ;; (ql:quickload "numery")
 ;; (in-package :numery)
