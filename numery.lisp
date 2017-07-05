08485ac8b7d5f4c72cf19b0b1c0fcd6f34f1460c
Ignacy Moryc <imoryc@protonmail.com>
2017-07-05 12:22:27 +0200

    All tests passing for relative and absolute errors

--- numery.lisp
+++ numery.lisp
@@ -11,15 +11,19 @@
       (coerce approximation 'double-float))))
 
 (define-test test-absolute-error
-  (assert-float-equal 0.0d0 (absolute-error 10 10))
-  (assert-float-equal 0.5d0 (absolute-error -10 -9.5))
-  (assert-float-equal 0.1d0 (absolute-error 10 9.9)))
+  (setq *print-failures* t)
+  (assert-true (= 0.0d0 (absolute-error 10 10)))
+  (assert-true (= 0.5d0 (absolute-error -10 -9.5)))
+  (assert-true (< 0.1 (absolute-error 10 9.9) 0.15)))
 
 (defun relative-error (absolute-error approximation)
   "Return the relative error between two numbers"
-  (abs (if (or (zerop exact) (zerop approximate))
-           (- exact approximate)
-           (/ (- exact approximate) exact))))
+  (abs (/ absolute-error approximation)))
+
+(define-test test-relative-error
+  (setq *print-failures* t)
+  (assert-true (< 0.0105 (relative-error 0.02d0 1.9d0) 0.0106))
+  (assert-true (< 0.0444 (relative-error 0.08d0 1.8d0) 0.0445)))
 
 ;; To run tests in SLIME use
 ;; (ql:quickload "numery")
